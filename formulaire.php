77fed3e273b11d8df75470639496f3694c7e3577
Winter <144462967+Winter948@users.noreply.github.com>
2024-04-04 15:28:33 +0200

    Version 1
    + Formulaire

---
+++ formulaire.php
@@ -0,0 +1,16 @@
+<form action="action.php" method="post">
+    <ul>
+        <li>
+            <label for="name">Nom :</label>
+            <input type="text" id="name" name="user_name" />
+        </li>
+        <li>
+            <label for="mail">E-mail :</label>
+            <input type="email" id="mail" name="user_mail" />
+        </li>
+        <li>
+            <label for="msg">Message :</label>
+            <textarea id="msg" name="user_message"></textarea>
+        </li>
+    </ul>
+</form>
